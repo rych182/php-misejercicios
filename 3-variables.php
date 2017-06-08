a229bb6b2555238b579cfe9db42fbbc875030111
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-08 16:18:42 -0500

    This is a example about variable

---
+++ 3-variables.php
@@ -0,0 +1,12 @@
+<?php 
+	$saludo = "Hola mundo";
+	$salto = "<br>";
+	$explicacion = "Para declarar una variable debes usar el signo $ y agregar el nombre.";
+	$explicacion2 = "Debes usar el signo de igual y poner entre comillas lo que quieres que lleve la variable si es una cadena de texto";
+	$explicacion3 = "No olvides poner el ; al terminar de declarar una variable";
+	$explicacion4 = "Para concatenar osea unir las cosas debes usar un punto asi: .";
+	$explicacion5 = "Las variables siempre deben de empezar de  preferencia con una letra, también se pueden almacenar numeros en las variables";
+
+	echo $saludo . $salto . $explicacion . $salto . $explicacion2 . $salto . $explicacion3 . $salto . $explicacion4;
+	echo $salto . $explicacion5;
+?>
