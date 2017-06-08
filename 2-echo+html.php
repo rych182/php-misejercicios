07de43ac5f379db19993f566927bce9951ba0bdf
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-08 04:27:13 -0500

    I added a new example about like use html on php

---
+++ 2-echo+html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Echo con HTML</title>
+</head>
+<body>
+	<?php 
+		echo "También puedes poner html dentro del php";
+		echo "<br>";
+		echo "Fijate en el código, utilice br de html para un salto de linea";
+		echo "<br>";
+		echo "<h1>Aquí estoy utilizando una etiqueta h1 de html</h1>";
+
+	?>
+</body>
+</html>
