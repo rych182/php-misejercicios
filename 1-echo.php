abedbfd7eb3a83ca6cf0d51994ffc61cf78d7a40
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-08 04:03:01 -0500

    explain like use echo in php

---
+++ 1-echo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>echo en PHP</title>
+</head>
+<body>
+	<h1><?php echo "Hola mundo"; ?></h1>
+	<br>
+	<h2> <?php echo "Este es otro Hola  mundo en PHP"; ?> </h2>
+	<br>
+	<h3> <?php echo "Para imprimir uso un echo en PHP";?> </h3>
+	<br>
+	<h4> <?php echo "Se puede mezclar el php junto con el HTML";?> </h4>
+</body>
+</html>
