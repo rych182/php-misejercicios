76f2e31efd08c747dd502e5fa901ae99bf429353
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-26 17:40:09 -0500

    'strlen' password para wordpress

--- 11-mayusculas-minusculas.php
+++ 11-mayusculas-minusculas.php
@@ -2,6 +2,14 @@
 	$nombre = "ricardo";
 	$apellido = "GARRIDO";
 	echo strtoupper($nombre) . "<br>";
-	echo strtolower($apellido);
+	echo strtolower($apellido) . "<br>";
+
+	$password = "123";
+
+	if (strlen($password)< 8) {
+		echo "La contraseña es muy debil";
+	}else {
+		echo "Tu contraseña es segura!!";
+	}
 
 ?>
