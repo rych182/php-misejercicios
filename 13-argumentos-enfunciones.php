d50321b0f444b62a05b3352a5a62d8dabecf2e3c
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-26 21:28:18 -0500

    Creando funciones mas chingonas

---
+++ 13-argumentos-enfunciones.php
@@ -0,0 +1,19 @@
+<?php
+
+	function saludar($nombre){
+		echo "Hola me llamo {$nombre} <br>";
+	}
+	saludar("Ricardo");
+	saludar("Perengano");
+	saludar("Sutano");
+	saludar("Julian");
+
+	function suma($num1, $num2){
+		echo $num1 + $num2 . "<br>";
+	}
+	suma(20,30);
+	suma(10,40);
+	suma(8,10);
+	suma(1,9);
+
+?>
